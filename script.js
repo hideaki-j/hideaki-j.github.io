92970d53f548b80edcddb2675b2d571e3e22b3a2
hideaki-j <hj.hideaki.joko@gmail.com>
2025-10-26 10:52:20 -0400

    Update scroll animation observer rootMargin to improve visibility on entry

--- script.js
+++ script.js
@@ -1086,7 +1086,7 @@ function initScrollAnimations() {
 
     const observerOptions = {
         threshold: 0.1,
-        rootMargin: '0px 0px -50px 0px'
+        rootMargin: '0px 0px 0px 0px'
     };
 
     const observer = new IntersectionObserver(entries => {
