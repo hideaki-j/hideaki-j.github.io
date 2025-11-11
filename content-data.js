088e74224cf0967eec0a37e19f8ec872d2a2cac8
hideaki-j <hj.hideaki.joko@gmail.com>
2025-11-11 07:19:56 -0800

    Update citation counts to reflect recent metrics

--- content-data.js
+++ content-data.js
@@ -383,23 +383,23 @@ window.siteContent = {
                     },
                     citations: {
                         firstAuthor: {
-                            citations: 99
+                            citations: 100
                         },
                         all: {
-                            citations: 103,
+                            citations: 104,
                             hIndex: 4,
                             i10Index: 3
                         },
                         since2020: {
-                            citations: 99,
+                            citations: 100,
                             hIndex: 4,
                             i10Index: 3
                         },
                         byYear: {
                             years: ["2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"],
-                            counts: [2, 0, 1, 1, 13, 19, 22, 38],
-                            firstAuthor: [2, 0, 1, 1, 13, 19, 22, 38],
-                            all: [2, 0, 1, 1, 13, 19, 22, 44]
+                            counts: [2, 0, 1, 1, 13, 19, 22, 39],
+                            firstAuthor: [2, 0, 1, 1, 13, 19, 22, 39],
+                            all: [2, 0, 1, 1, 13, 19, 22, 45]
                         }
                     },
                     coauthors: [
@@ -470,7 +470,7 @@ window.siteContent = {
                         title: "Doing personal laps: Llm-augmented dialogue construction for personalized multi-session conversational search",
                         authors: "H Joko, S Chatterjee, A Ramsay, AP De Vries, J Dalton, F Hasibi",
                         venue: "SIGIR '24",
-                        cited_by: 43,
+                        cited_by: 44,
                         year: 2024,
                         ranking: "A*",
                         url: "https://dl.acm.org/doi/abs/10.1145/3626772.3657815",
